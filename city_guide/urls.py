c2b4b24304d0a84329ba366e705a1635803234cc
ilyashirko <ilyashirko@gmail.com>
2022-08-22 17:51:37 +0300

    repaired static path in urls

--- city_guide/urls.py
+++ city_guide/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from where_to_go.views import main_page, place_via_id
-
-from . import settings
+from . import settings 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,7 +13,7 @@ urlpatterns = [
 
 urlpatterns += static(
     settings.STATIC_URL,
-    document_root=settings.STATICFILES_DIRS
+    document_root=settings.STATICFILES_DIRS if settings.DEBUG else settings.STATIC_ROOT
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
