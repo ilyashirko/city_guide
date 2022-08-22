5b69baed3a4ae24a87b0b6359fe11464dce33eeb
ilyashirko <ilyashirko@gmail.com>
2022-08-22 17:37:17 +0300

    change stati folder settings

--- city_guide/settings.py
+++ city_guide/settings.py
@@ -118,9 +118,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_PATH)
 IMAGES_PATH = 'images'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
-]
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
